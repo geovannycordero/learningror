04bdcc05779d1c5c69d9c331dd077e02074affac
Geovanny Cordero <geovanny@pm.me>
2019-12-09 10:07:13 -0600

    32, Added new and create portafolio

--- app/controllers/portafolios_controller.rb
+++ app/controllers/portafolios_controller.rb
@@ -2,4 +2,20 @@ class PortafoliosController < ApplicationController
   def index
     @portafolio_items = Portafolio.all
   end
+
+  def new
+    @portafolio_item = Portafolio.new
+  end
+
+  def create
+    @portafolio_item = Portafolio.new(params.require(:portafolio).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @portafolio_item.save
+        format.html { redirect_to portafolios_path, notice: 'Portafolio was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
 end
